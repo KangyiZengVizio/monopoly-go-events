0dc3f3f1f9fb7759e78548377a65a50ce98b46c5
KangyiZengVizio <kangyi.zeng@vizio.com>
2024-04-25 23:43:54 -0500

    update

--- text_message.py
+++ text_message.py
@@ -38,9 +38,13 @@ def send_message(phone_number, carrier, message):
  
 if __name__ == "__main__":
 
-    phone_number = config['recipient 1']['phone']
-    carrier = config['recipient 1']['carrier']
-    message1 = f"\nEvent {Title} will begin, At {Time},duration is {Duration}".encode('utf-8')
-    print(message1)
+    recipient_1 = config['recipient 1']
+    recipient_2 = config['recipient 2']
+    recipients=[recipient_1,recipient_2]
+    message = f"\nEvent {Title} will begin, At {Time},duration is {Duration}".encode('utf-8')
+    print(message)
+    for re in recipients:
+        send_message(re['phone'], re['carrier'], message)
+    
     #send_message(phone_number, carrier, message1)
-    send_message(phone_number, carrier, message1)
+    
